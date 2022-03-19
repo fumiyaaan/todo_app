9182d2aa50ab91b3d71b8cf6fcdfd731490d785a
fumiyaaan <indestructible.peace@gmail.com>
2022-03-19 20:41:12 +0900

    DB作成

--- main.go
+++ main.go
@@ -3,12 +3,16 @@ package main
 import (
 	"fmt"
 
-	"todo_app/config"
+	"todo_app/app/models"
 )
 
 func main() {
-	fmt.Println(config.Config.Port)
-	fmt.Println(config.Config.SQLDriver)
-	fmt.Println(config.Config.DbName)
-	fmt.Println(config.Config.Logfile)
+	/*
+		fmt.Println(config.Config.Port)
+		fmt.Println(config.Config.SQLDriver)
+		fmt.Println(config.Config.DbName)
+		fmt.Println(config.Config.Logfile)
+	*/
+
+	fmt.Println(models.Db)
 }
