37cfbf95571e94af6a3fca90436a68e38a494448
fumiyaaan <indestructible.peace@gmail.com>
2022-03-19 12:24:50 +0900

    フィールド名のスペルミスを修正

--- main.go
+++ main.go
@@ -10,5 +10,5 @@ func main() {
 	fmt.Println(config.Config.Port)
 	fmt.Println(config.Config.SQLDriver)
 	fmt.Println(config.Config.DbName)
-	fmt.Println(config.Config.LoadFile)
+	fmt.Println(config.Config.Logfile)
 }
